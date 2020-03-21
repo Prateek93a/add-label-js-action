f2e86fae516a547a19458f315b3acf71fa0fe72f
prateek93a <prateek93a@gmail.com>
2020-03-21 22:15:24 +0530

    heee

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const token = process.env.Github;
+if(!token){
+  return;
+}
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  const octokit = new github.GitHub(token);
+  const pr = github.context.payload['pull_request'];
+  if(!pr) return
+  if(pr.body){
+   console.log(pr.body)
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
